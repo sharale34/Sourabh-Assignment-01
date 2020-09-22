6a1091d8ccec53314897f476712ef689cd128a64
sharale34 <sourabhharale8000@gmail.com>
2020-09-22 17:13:37 +0530

    Daily Wage Calculation

--- EmpWage.java
+++ EmpWage.java
@@ -7,21 +7,25 @@ public class EmpWage {
 		
 		//constants
 		int IS_FULL_TIME=1;
+		int EMP_WAGE_PER_HOUR=20;
+		
+		//variables
+		int empWage=0;
+		int empHours=0;
 		
 	   //computation
-		double empCheck = Math.floor(Math.random()*10) % 2;
-		System.out.println("random generation :"+Math.random());
-		System.out.println("random generation :"+Math.random()*10);
-		System.out.println("random generation :"+Math.floor(Math.random()*10));
-		System.out.println("random generation :"+Math.floor(Math.random()*10) % 2);
+		double empCheck = Math.floor(Math.random()*10) % 3;
 		if(empCheck == IS_FULL_TIME)
 		{
+			empHours=8;
 			System.out.println("Employee is Present");
 		}
 		else
 		{
 			System.out.println("Employee is Absent");
 		}
+		empWage= empHours * EMP_WAGE_PER_HOUR;
+		System.out.println("Employee Wage :"+empWage);
 
 	}
 
