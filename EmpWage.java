66bf9b801b3e1ee34ab406c7364130fe909af0a0
sharale34 <sourabhharale8000@gmail.com>
2020-09-22 16:47:12 +0530

    check Emp present/absent

--- EmpWage.java
+++ EmpWage.java
@@ -3,7 +3,25 @@ package WageComp;
 public class EmpWage {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
+		//Auto-generated method stud
+		
+		//constants
+		int IS_FULL_TIME=1;
+		
+	   //computation
+		double empCheck = Math.floor(Math.random()*10) % 2;
+		System.out.println("random generation :"+Math.random());
+		System.out.println("random generation :"+Math.random()*10);
+		System.out.println("random generation :"+Math.floor(Math.random()*10));
+		System.out.println("random generation :"+Math.floor(Math.random()*10) % 2);
+		if(empCheck == IS_FULL_TIME)
+		{
+			System.out.println("Employee is Present");
+		}
+		else
+		{
+			System.out.println("Employee is Absent");
+		}
 
 	}
 
