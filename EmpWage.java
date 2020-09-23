186bf12ff41f6a1bc8c4414f1f4b6a12c1914c26
sharale34 <sourabhharale8000@gmail.com>
2020-09-23 11:50:52 +0530

    Wages till a condition of total working hrs

--- EmpWage.java
+++ EmpWage.java
@@ -10,6 +10,7 @@ public class EmpWage {
 		final int IS_PART_TIME=2;
 		final int EMP_WAGE_PER_HOUR=20;
 		final int DAYS_IN_MONTH=20;
+		final int WORKHOURS_PER_MONTH=100;
 		
 		//variables
 		int empWage=0;
@@ -23,19 +24,29 @@ public class EmpWage {
 		switch(empCheck)
 		{
 		   case IS_PART_TIME:
+			   System.out.println("Employee is Present & Full Time");
 			   empHours=8;
 			   break;
 			   
 		   case IS_FULL_TIME:
+			   System.out.println("Employee is Present & Part Time");
 			   empHours=8;
 			   break;
 			   
 		   default:
+			   System.out.println("Employee is Abesent");
 			   empHours=0;
 		}
-		empWage= empHours * EMP_WAGE_PER_HOUR;
-		empMonthWage=empWage * DAYS_IN_MONTH;
-		System.out.println("Employee Monthly Wage :"+empMonthWage);
+		if(WORKHOURS_PER_MONTH/empHours<DAYS_IN_MONTH && empHours!=0)
+		{
+		empWage= WORKHOURS_PER_MONTH * EMP_WAGE_PER_HOUR;
+		System.out.println("Employee Wage for 100 hrs :"+empWage);
+		}
+		else if(WORKHOURS_PER_MONTH/empHours >=DAYS_IN_MONTH && empHours!=0)
+		{
+			empWage= empHours * WORKHOURS_PER_MONTH * EMP_WAGE_PER_HOUR;
+			System.out.println("Employee Wage for 20 days :"+empWage);
+		}
 
 	}
 
