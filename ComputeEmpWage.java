24a4d3a643a6ea4c5259c6d5a40234d0eb8172e1
sharale34 <sourabhharale8000@gmail.com>
2020-09-29 08:42:01 +0530

    Refactor: Refactor to Have One EmpWageBuilder to Implement Interface

---
+++ ComputeEmpWage.java
@@ -0,0 +1,9 @@
+package WageComp;
+
+public interface ComputeEmpWage {
+	
+	public void computeWage();
+
+	public void addCompanyEmpWage(int empWagePerHour, int workDaysPerMonth, int workHoursPerMonth, String companyName);
+}
+
